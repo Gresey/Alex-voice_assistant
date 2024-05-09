0765255c0255185a08afb63344275dfc89e872ec
Gresey Patidar <greseypatidar100@gmail.com>
2024-05-09 09:52:01 +0530

    Code updated

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const btn=document.querySelector('.talk')
 const content=document.querySelector('.content')
 
-function speak(text){                         //speak the text
+function speak(text){                         // vspeak the text
    const text_speak= new SpeechSynthesisUtterance(text);
    text_speak.rate=1;
    text_speak.volume=1;
